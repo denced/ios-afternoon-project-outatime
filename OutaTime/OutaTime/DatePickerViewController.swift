cb50d36897ffe9b1d0719686539c32a0c437d4e3
Denis Cedeno <denis.cedeno@gmail.com>
2019-10-09 17:25:02 -0400

    completed project

--- OutaTime/OutaTime/DatePickerViewController.swift
+++ OutaTime/OutaTime/DatePickerViewController.swift
@@ -14,34 +14,25 @@ protocol DatePickerDelegate{
 
 class DatePickerViewController: UIViewController {
     
-    @IBOutlet weak var datePicker: UIPickerView!
+    var delegate: DatePickerDelegate?
     
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+
     @IBAction func cancelButton(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
-    
     @IBAction func doneButton(_ sender: Any) {
-        delegate?.destinationDateWasChosen(<#T##date: Date##Date#>)
+        delegate?.destinationDateWasChosen(datePicker.date)
         dismiss(animated: true, completion: nil)
     }
     
-    var delegate: DatePickerDelegate?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
+
