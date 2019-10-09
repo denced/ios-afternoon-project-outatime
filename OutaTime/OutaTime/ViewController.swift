1526cd7a9bf15bb86e7efc4135c377c4a694e4ff
Denis Cedeno <denis.cedeno@gmail.com>
2019-10-08 20:47:42 -0400

    created new project

---
+++ OutaTime/OutaTime/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OutaTime
+//
+//  Created by denis cedeno on 10/8/19.
+//  Copyright © 2019 DenCedeno Co. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
