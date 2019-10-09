d8f8528e94ef1145008444dd263584a7034e6491
Denis Cedeno <denis.cedeno@gmail.com>
2019-10-08 22:09:43 -0400

    created and wired the story board

---
+++ OutaTime/OutaTime/DatePickerViewController.swift
@@ -0,0 +1,37 @@
+//
+//  DatePickerViewController.swift
+//  OutaTime
+//
+//  Created by denis cedeno on 10/8/19.
+//  Copyright © 2019 DenCedeno Co. All rights reserved.
+//
+
+import UIKit
+
+class DatePickerViewController: UIViewController {
+    
+    @IBOutlet weak var datePicker: UIPickerView!
+    
+    @IBAction func cancelButton(_ sender: Any) {
+    }
+    
+    @IBAction func doneButton(_ sender: Any) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ OutaTime/OutaTime/TimeCircuitsViewController.swift
@@ -0,0 +1,40 @@
+//
+//  TimeCircuitsViewController.swift
+//  OutaTime
+//
+//  Created by denis cedeno on 10/8/19.
+//  Copyright © 2019 DenCedeno Co. All rights reserved.
+//
+
+import UIKit
+
+class TimeCircuitsViewController: UIViewController {
+
+    @IBOutlet weak var lastTimeDepartedLabel: UILabel!
+    @IBOutlet weak var presentTimeLabel: UILabel!
+    @IBOutlet weak var destinationLabel: UILabel!
+    @IBOutlet weak var speedMPHlabel: UILabel!
+    @IBAction func travelBackButton(_ sender: Any) {
+    }
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
